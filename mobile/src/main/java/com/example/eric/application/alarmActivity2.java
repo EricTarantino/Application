bdb116970e68cd46faaaf719aba84ffedd8061e9
e.lessen <eric.vanlessen@live.de>
2016-02-15 16:12:28 +0100

    Commit 03.02

--- mobile/src/main/java/com/example/eric/application/alarmActivity2.java
+++ mobile/src/main/java/com/example/eric/application/alarmActivity2.java
@@ -7,10 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -19,7 +16,8 @@ public class alarmActivity2 extends AppCompatActivity {
     ProzessProvider processProvider;
     int [] processes =processProvider.getProcesses();
     int processCount = 0;
-    int processCurrent = 0;
+    int processCurrentId = 0;
+    int processCurrentDuration = 0;
     int zeit = 0;
     Timer caretaker;
 
@@ -31,29 +29,32 @@ public class alarmActivity2 extends AppCompatActivity {
     //processProvider = new ProzessProvider();
     }
 
-    // Diese Methoden schreiben:
+    //  Diese Methoden schreiben:
     //  Timer Setzen
     //  LOOP 0 <= i < 4
-    //  Bei Click auf Prozess starten Button
-    //  Timer i starten
-    //  Zeitstring mit Timer i Zeit den Wert von Timer i geben
-    //  Update den Timerstring jede Sekunde
+    //  onClick_Prozess starten
+    //  Prozess_starten ausblenden und starten des Prozesses
     //  Counter weniger als 10 sec: Show Prozess Button erhält einen Background (Rahmen)
     //  Counter abgelaufen: Prozess  i läuft wird invisible
     //  Show Prozess Button Background ausblenden (Kein Rahmen)
     //  Prozess i um eins erhöhen
     //  Prozess läuft Button: Text anpassen auf Prozess i läuft
     //  Show: Prozess starten Button text auf Prozess i setzen
+    //  Bei Click auf Prozess starten Button, Process i starten
+    //  Feld Prozess läuft den Wert von Timer i geben
 
     public void onClickButton_Prozess_starten(View view) {
         Button button_Prozess_starten = (Button)findViewById(R.id.button_Prozess_starten);
         button_Prozess_starten.setVisibility(View.INVISIBLE);
 
-        int duration = startProcess();
+        processCurrentDuration = startProcess();
 
         Button button_Prozess_läuft = (Button)findViewById(R.id.button_Prozess_läuft);
-        button_Prozess_läuft.setText("Prozess "+ (processCount+1) +" läuft. &lt;br /&gt; &lt;br /&gt Zweite Zeile");
+        button_Prozess_läuft.setText("Prozess "+ (processCount+1) +" läuft. &lt;br /&gt; &lt;br /&gt"+ processCurrentDuration);
         button_Prozess_läuft.setVisibility(View.VISIBLE);
+
+        //  Update den Timerstring jede Sekunde
+        startSecond();
     }
 
     private int startProcess() {
@@ -61,16 +62,17 @@ public class alarmActivity2 extends AppCompatActivity {
         TimerTask action = new TimerTask() {
             public void run() {
                 Message msg = handlerProcessEnd.obtainMessage();
-                msg.arg1 = processCount+1;
                 handlerProcessEnd.sendMessage(msg);
             }
         };
         caretaker = new Timer();
+        // Process i starten
         caretaker.schedule(action, processes[processCount]);
 
         return  processes[processCount];
     }
 
+    //  Update den Timerstring jede Sekunde
     private void startSecond() {
 
         TimerTask action = new TimerTask() {
@@ -83,31 +85,42 @@ public class alarmActivity2 extends AppCompatActivity {
         caretaker.schedule(action, 60000);
     }
 
+    //  Counter abgelaufen: Prozess  i läuft wird invisible
     Handler handlerProcessEnd = new Handler(){
         @Override
         public void handleMessage(Message msg) {
-
-            Button button = (Button) findViewById(R.id.id_button_alarm);
-            button.setBackgroundColor(0xffff0000);
-
-            switch(msg.arg1){
-
-            }
-
-            button.setVisibility(View.VISIBLE);
+            blendInStartNextProcess();
         }
     };
 
+    //  Update den Timerstring jede Sekunde
     Handler handlerSecondOver = new Handler(){
         @Override
         public void handleMessage(Message msg) {
 
-            Button button = (Button) findViewById(R.id.button_Prozess_läuft);
-            button.setText("Prozess "+ (processCount+1) +" läuft. &lt;br /&gt; &lt;br /&gt Zweite Zeile "+zeit);
             zeit+=1;
+            Button button = (Button) findViewById(R.id.button_Prozess_läuft);
+            button.setText("Prozess "+ (processCount+1) +
+                                    " läuft. &lt;br /&gt; &lt;br /&gt"+ (processCurrentDuration - zeit*10000));
             button.setVisibility(View.VISIBLE);
-            if(processCount==processCurrent)
+            if(processCount== processCurrentId) {
                 startSecond();
+            } else {
+                zeit = 0;
+            }
         }
     };
+
+    //  Counter abgelaufen: Prozess  i läuft wird invisible
+    //  Prozess i um eins erhöhen
+    //  Prozess läuft Button: Text anpassen auf Prozess i läuft
+    //  Show: Prozess starten Button text auf Prozess i setzen
+    public void blendInStartNextProcess(){
+        Button button_Prozess_läuft = (Button)findViewById(R.id.button_Prozess_läuft);
+        button_Prozess_läuft.setVisibility(View.INVISIBLE);
+        processCount += 1;
+        Button button_Prozess_starten = (Button)findViewById(R.id.button_Prozess_starten);
+        button_Prozess_starten.setText("Prozess "+ (processCount+1) +" starten");
+        button_Prozess_starten.setVisibility(View.VISIBLE);
+    }
 }
